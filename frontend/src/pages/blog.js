da6bfec95f8efc06e334aaee22e6d9aac10159b3
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2021-03-12 17:08:15 +0800

    Generate post titles from query

--- frontend/src/pages/blog.js
+++ frontend/src/pages/blog.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
 
-export default function BlogPage() {
+function PostList({posts}) {
   return (
     <div>
-      <div>This is the blog page!</div>
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li key={post.id}>
+              <Link to={post.slug.current}>{post.title}</Link>
+            </li>
+          )
+        }) }
+      </ul>
     </div>
-  )
+  );
 }
+
+export default function BlogPage({ data }) {
+  const posts = data.posts.nodes;
+  // console.log(posts);
+  return (
+    <PostList posts={posts} />
+  );
+}
+
+export const query = graphql`
+  query postTitleQuery {
+    posts: allSanityPost {
+      nodes {
+        title
+        id
+        slug {
+          current
+        }
+        categories {
+          title
+        }
+        author {
+          name
+        }
+        body {
+          children {
+            text
+          }
+        }
+      }
+    }
+  }
+`;
